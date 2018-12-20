4d01c7bbdb9a45ed86bb2a5540655d74ea174601
Luke Falcone <lfalcone@uci.edu>
2018-12-19 22:24:27 -0800

    Big push of assets added into wwise, will delete mirrored assest in project once all are integrated

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,42 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID COINPICIKUP = 1668078473U;
+        static const AkUniqueID CROCCRY = 1360483428U;
+        static const AkUniqueID CROCGRAB = 1767585834U;
+        static const AkUniqueID MUMMYCRY = 3864997638U;
+        static const AkUniqueID PLAYERHURT = 3537581393U;
+        static const AkUniqueID PLAYERPUNCH = 3312008230U;
+        static const AkUniqueID PLAYERSTEP = 412471812U;
+        static const AkUniqueID PLAYERSWIM = 3968782598U;
+        static const AkUniqueID SPITTERCRY = 1373860844U;
+        static const AkUniqueID STARTMUSIC = 3827058668U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace LEVEL
+        {
+            static const AkUniqueID GROUP = 2782712965U;
+
+            namespace STATE
+            {
+                static const AkUniqueID PEDESTAL = 2544092651U;
+                static const AkUniqueID RAFT = 1925960664U;
+                static const AkUniqueID TITLE = 3705726509U;
+            } // namespace STATE
+        } // namespace LEVEL
+
+    } // namespace STATES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
